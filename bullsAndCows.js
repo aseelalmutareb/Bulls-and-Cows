aef5b1419d6e9cc8d616cb8a0ee0c8c47f1205a1
Aseel <aseel.almutareb@gmail.com>
2022-03-10 15:13:12 +0100

    updated version

--- bullsAndCows.js
+++ bullsAndCows.js
@@ -1,6 +1,12 @@
 class BullsAndCows{
     constructor(){
         this.secretDigits = this.generateSecretDigits();
+        this.hints = {};
+        this.attempts = 0;
+        this.noLuckMessages = [
+            "Sorry, no luck this time",
+            ""
+        ];
     }
 
     validateInput(input){
@@ -19,7 +25,7 @@ class BullsAndCows{
     }
 
     compare(guess){
-       return guess.split("").reduce((hint, num, index) =>{
+       let hint = guess.split("").reduce((hint, num, index) =>{
            if(this.secretDigits.includes(num)){
              if(this.secretDigits.indexOf(num) === index)
                 hint.bull++;
@@ -28,10 +34,13 @@ class BullsAndCows{
            }
            return hint;
         }, {bull: 0, cow: 0}); 
+
+        this.hints = {...hint};
+        return hint;
     }
 
     getHints(){
-      return `Hint`;  
+      return `Hint: ${this.hints.bull} ${String.fromCodePoint(0x1F402)} and ${this.hints.cow} ${String.fromCodePoint(0x1F404)}`; 
     }
 }
 
--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const BullsAndCows = require("./bullsAndCows");
 
 
 const game = new BullsAndCows();
-let guess = prompt('Enter your 4 digits guess: ');
+let guess = "";
 
-while(!game.validateInput(guess)){
+
+const runGame = function(){
+    guess = prompt('Enter your 4 digits guess: ');
+    let isValid = game.validateInput(guess);
+    while(!isValid){
     guess = prompt('Re-enter a valid guess: ');
-    game.validateInput(guess);
+    isValid = game.validateInput(guess);
+    }
+    game.compare(guess);
+    console.log(game.getHints());
+    console.log(game.secretDigits);
 }
 
-    console.log(game.compare(guess), game.secretDigits);
+runGame();
+
+
+while(game.hints.bull !== 4){
+runGame(); }
+
+console.log("Congrats!! Your guess is correct!");
 
 
 
-    
 
-console.log(String.fromCodePoint(0x1F404, 0x1F402));
 
